20c5f77e625295e282a19acae6df2de80218c633
Haejin Yoon <hjyoomp@gmail.com>
2021-11-16 16:06:34 +0900

    211116 3rd lecture commit

---
+++ src/servlet09/S06BookListServlet.java
@@ -0,0 +1,56 @@
+package servlet09;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import sample03javabean.Bean06;
+
+/**
+ * Servlet implementation class S06BookListServlet
+ */
+@WebServlet("/servlet09/list")
+public class S06BookListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public S06BookListServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		HttpSession session = request.getSession();
+		List<Bean06> list = (List<Bean06>)session.getAttribute("books");
+		if(list==null) {
+			list= new ArrayList<>();
+			session.setAttribute("books", list);
+		}
+		 Bean06 book1 = new Bean06("servlet09에서 추가한 책", "kim", 20000, "apub", 24); 
+		 list.add(book1);
+		String path = "/WEB-INF/view/servlet08/list.jsp";
+		request.getRequestDispatcher(path).forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
